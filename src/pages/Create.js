5f0f06362d5ae214a7d887139cc0b7fa276230a5
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2024-01-30 20:19:13 +0200

    Text Fields

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,8 +1,30 @@
-import React from "react";
-import { Button, Container, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Button, Container, TextField, Typography } from "@mui/material";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 export default function Create() {
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+
+  const [isTitleError, setIsTitleError] = useState(false);
+  const [isDetailsError, setIsDetailsError] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setIsTitleError(!title);
+    setIsDetailsError(!details);
+
+    if (!title || !details) {
+      return;
+    }
+
+    console.log(title, details);
+
+    setTitle('');
+    setDetails('');
+  };
+
   return (
     <Container>
       <Typography
@@ -14,14 +36,48 @@ export default function Create() {
         Create a New Note
       </Typography>
 
-      <Button
-        type="submit"
-        color="primary"
-        variant="contained"
-        endIcon={<KeyboardArrowRightIcon />}
-      >
-        Submit
-      </Button>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          sx={{
+            my: 2,
+            display: "block",
+          }}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          error={isTitleError}
+        />
+
+        <TextField
+          value={details}
+          onChange={(event) => setDetails(event.target.value)}
+          sx={{
+            my: 2,
+            display: "block",
+          }}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={isDetailsError}
+        />
+
+        <Button
+          type="submit"
+          color="secondary"
+          variant="contained"
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 }
