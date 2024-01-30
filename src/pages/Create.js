4f7f75033f3de429a557f0ed4d9ee427ac6a7632
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2024-01-30 17:15:55 +0200

    Typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Typography } from "@mui/material";
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography
+        variant="h6"
+        component="h1"
+        color="textSecondary"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
     </div>
-  )
+  );
 }
