78834019e74fa12c6b0d13b160b635461931a41c
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2024-01-30 22:34:30 +0200

    Grid System

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import { localClient } from "../localClient";
+import { Container, Grid, Paper } from "@mui/material";
 
 export default function Notes() {
   const [notes, setNotes] = useState(localClient.init("notes", []));
 
   return (
-    <div>
-      {notes.map((note) => (
-        <p key={note.id}>{note.title}</p>
-      ))}
-    </div>
+    <Container>
+      <Grid container>
+        {notes.map((note) => (
+          <Grid key={note.id} item xs={12} sm={6} md={4} lg={3}>
+            <Paper>{note.title}</Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 }
