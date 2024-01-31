136b14616792118381770f4a110fe1507aec492a
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2024-01-31 13:41:13 +0200

    Permanent Drawer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,35 @@
-import { Box } from "@mui/material";
+import { Box, Drawer, Typography } from "@mui/material";
 import React from "react";
 
 const classes = {
+  root: {
+    display: "flex",
+  },
   page: {
     backgroundColor: "#f9f9f9",
     width: "100%",
+  },
+  drawer: {
+    width: 240,
   },
 };
 
 export const Layout = ({ children }) => {
   return (
-    <Box>
+    <Box sx={classes.root}>
       {/* app bar */}
 
       {/* side drawer */}
+      <Drawer
+        sx={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        PaperProps={{ sx: classes.drawer }}
+      >
+        <Typography variant="h5">
+          Menu
+        </Typography>
+      </Drawer>
 
       <Box sx={classes.page}>
         {children}
