9fb595d06d9ef408af36a7aab8032385145127bc
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2024-01-31 17:27:43 +0200

    add gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { Notes } from "./pages/Notes";
 import { Create } from "./pages/Create";
 import { ThemeProvider, createTheme } from "@mui/material";
